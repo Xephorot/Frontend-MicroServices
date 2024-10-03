72046e9618bc7bfe413eddefd9a6be1096724e03
Sergio Escalante <escalantesergio821@gmail.com>
2024-10-03 14:12:40 -0400

    Limpiado de codigo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import AdminView from './views/AdminView';
-import MainView from './views/MainView';
-import ProductForm from './views/ProductForm';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AdminView from "./views/AdminView";
+import MainView from "./views/MainView";
+import ProductForm from "./views/ProductForm";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-    return (
-        <Router>
-            <div className="container-fluid">
-                <Routes>
-                    <Route path="/admin" element={<AdminView />} />
+  return (
+    <Router>
+      <div className="container-fluid">
+        <Routes>
+          <Route path="/admin" element={<AdminView />} />
 
-                    <Route path="/admin/new" element={<ProductForm onSave={() => {}} />} />
+          <Route
+            path="/admin/new"
+            element={<ProductForm onSave={() => {}} />}
+          />
 
-                    <Route path="/admin/edit/:id" element={<ProductForm onSave={() => {}} />} />
+          <Route
+            path="/admin/edit/:id"
+            element={<ProductForm onSave={() => {}} />}
+          />
 
-                    <Route path="/" element={<MainView />} />
-                </Routes>
-            </div>
-        </Router>
-    );
+          <Route path="/" element={<MainView />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
